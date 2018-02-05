4281c1e1c649f167b252ec16e23f87dd3083847d
sweeneyb <tab126@gmail.com>
2018-02-04 22:41:00 -0600

    clean up useless commments

--- src/main/kotlin/com/sweeneyb/Application.kt
+++ src/main/kotlin/com/sweeneyb/Application.kt
@@ -1,28 +1,16 @@
 package com.sweeneyb
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.hateoas.PagedResources
-import org.springframework.hateoas.Resource
-import org.springframework.hateoas.Resources
-import org.springframework.hateoas.UriTemplate
 import org.springframework.hateoas.hal.Jackson2HalModule
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import org.springframework.hateoas.core.DefaultRelProvider
-import org.springframework.hateoas.hal.DefaultCurieProvider
-import org.springframework.hateoas.hal.CurieProvider
-import org.springframework.http.*
-
 
 open class DataRestApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
-//            SpringApplication.run(DataRestApplication::class.java, *args)
             DataRestApplication().doWork()
         }
     }
@@ -33,37 +21,14 @@ open class DataRestApplication {
         mapper.registerModule(Jackson2HalModule())
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         val messageConverter = MappingJackson2HttpMessageConverter()
-
-        messageConverter.supportedMediaTypes = MediaType.parseMediaTypes("application/hal+json")
         messageConverter.objectMapper = mapper
-//
-//        val curieProvider = DefaultCurieProvider("a", UriTemplate("http://localhost:8080/menus/{rel}"))
-//        mapper.setHandlerInstantiator(Jackson2HalModule.HalHandlerInstantiator(DefaultRelProvider(), curieProvider, null))
-//
+
         val restTemplate = RestTemplate(listOf(messageConverter))
         val uri = "http://localhost:8080/menus"
-//        val restTemplate = RestTemplate()
-//        val resources = PagedResources<Menu>(){}()
-//            val quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote::class.java)
-//        val menu = restTemplate.getForEntity("http://localhost:8080/menus", Menu::class.java)
-//        val re = restTemplate.getForEntity("http://localhost:8080/menus", Foo::class.java)
-//        val re = restTemplate.getForObject("http://localhost:8080/menus", Foo::class.java)
-//        val re = restTemplate.getForEntity("http://localhost:8080/menus", Bar::class.java).body
-        val re = restTemplate.exchange(uri, HttpMethod.GET, HttpEntity.EMPTY, Foo::class.java)
-//        println(re.body)
-//        println(re.body.content.forEach({println(it!!::class.java.name)}))
+        val re = restTemplate.getForEntity("http://localhost:8080/menus", Foo::class.java)
         println(re)
-//        val content = re.content
-//        println(content)
-
+        re.body.content.forEach({ println(it.menu.title) })
     }
 
-
-//    class Foo<MenuSupport>() : PagedResources<MenuSupport >() {    }
-    class Foo<Menu>() : PagedResources<MenuSupport >() {    }
-
-    class Bar<Menu>() : ParameterizedTypeReference<PagedResources<Menu>>() {
-
-    }
-
+    class Foo<Menu> : PagedResources<MenuSupport>()
 }
--- src/main/kotlin/com/sweeneyb/Menu.kt
+++ src/main/kotlin/com/sweeneyb/Menu.kt
@@ -3,35 +3,24 @@ package com.sweeneyb
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonUnwrapped
 import org.springframework.hateoas.ResourceSupport
-import org.springframework.hateoas.config.EnableHypermediaSupport
-import java.time.LocalDate
 import java.util.*
-import javax.annotation.Resource
 
 class MenuSupport : ResourceSupport() {
     @JsonUnwrapped
-    var menu : Menu = Menu()
+    var menu: Menu = Menu()
 }
 
-//@EnableHypermediaSupport(type = [EnableHypermediaSupport.HypermediaType.HAL])
-//@Resource
 class Menu {
 
     var id: Long? = null
 
-//    var restaurant: Restaurant? = null
-
-//    var date: LocalDate? = null
     @JsonProperty("date2")
-      var date: Date? = null
-//    var date2: Date? = null
+    var date: Date? = null
 
     var title: String? = null
 
     var text: String? = null
 
-//        override fun toString() = "Menu($id, ${restaurant?.id}, $date, $date2, $title, $text)"
-//    override fun toString() = "Menu($id, $date, $date2, $title, $text)"
     override fun toString() = "Menu($id,  $date, $title, $text)"
 
     override fun equals(other: Any?): Boolean {
@@ -50,7 +39,6 @@ class Menu {
 
     override fun hashCode(): Int {
         var result = id?.hashCode() ?: 0
-//        result = 31 * result + (restaurant?.id?.hashCode() ?: 0)
         result = 31 * result + (date?.hashCode() ?: 0)
         result = 31 * result + (title?.hashCode() ?: 0)
         result = 31 * result + (text?.hashCode() ?: 0)
