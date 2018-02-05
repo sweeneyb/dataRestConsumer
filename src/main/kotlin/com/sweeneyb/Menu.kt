fb583bca6165ce9a3f193dcb718a9b7771c81110
sweeneyb <tab126@gmail.com>
2018-02-04 21:59:01 -0600

    adds initial files; pulls Entity as a linkedList

---
+++ src/main/kotlin/com/sweeneyb/Menu.kt
@@ -0,0 +1,56 @@
+package com.sweeneyb
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped
+import org.springframework.hateoas.ResourceSupport
+import org.springframework.hateoas.config.EnableHypermediaSupport
+import java.time.LocalDate
+import java.util.*
+import javax.annotation.Resource
+
+class MenuSupport : ResourceSupport() {
+    @JsonUnwrapped
+    var menu : Menu = Menu()
+}
+
+//@EnableHypermediaSupport(type = [EnableHypermediaSupport.HypermediaType.HAL])
+//@Resource
+class Menu : ResourceSupport(){
+
+    var id: Long? = null
+
+//    var restaurant: Restaurant? = null
+
+    var date: LocalDate? = null
+
+    var date2: Date? = null
+
+    var title: String? = null
+
+    var text: String? = null
+
+    //    override fun toString() = "Menu($id, ${restaurant?.id}, $date, $date2, $title, $text)"
+    override fun toString() = "Menu($id, $date, $date2, $title, $text)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Menu
+
+        if (id != other.id) return false
+        if (date != other.date) return false
+        if (title != other.title) return false
+        if (text != other.text) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+//        result = 31 * result + (restaurant?.id?.hashCode() ?: 0)
+        result = 31 * result + (date?.hashCode() ?: 0)
+        result = 31 * result + (title?.hashCode() ?: 0)
+        result = 31 * result + (text?.hashCode() ?: 0)
+        return result
+    }
+}
